9d70501818bc3a6d1ea1a67730a9ffb1a9fea909
Rachana-arch <67087704+Rachana-arch@users.noreply.github.com>
2020-07-09 21:47:28 +0530

    Create createclass.js

---
+++ createclass.js
@@ -0,0 +1,16 @@
+<html>
+<p id="demo"></p>
+<script>
+class Car{
+constructor(name){
+this.name=name;
+}
+}
+
+obj=new Car("Farari")
+document.getElementById("demo").innerHTML= obj.name;
+</script>
+<body>
+</body>
+
+</html>
